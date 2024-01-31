77804aeb75d923aea287d759304642235067b175
adem0444 <ademziraoui6@gmail.com>
2024-01-31 20:57:17 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,119 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className='title'>
+  <h1>FIFA23</h1>
+ <h2>OFFICIAL CARD DESIGNS</h2>
+ </div>
+ <>
+ <div className='body'>
+  <nav className="bg-dark navbar-dark">
+    <div className="container">
+      <a href="" className="navbar-brand">
+        <i className="fas fa-tree mr-2" />
+        ULTIMATE TEAM
+      </a>
+    </div>
+  </nav>
+  <section id="header" className="jumbotron text-center">
+    <h1 className="display">FLASH BACK ITEM</h1>
+  </section>
+  <section id="gallery">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4 mb-4">
+          <div className="card">
+            <img
+              src="https://metro.co.uk/wp-content/uploads/2022/11/lionel-messi-flashback-sbc-fut-copy-5ce3.jpg?quality=90&strip=all"
+              alt=""
+              className="card-img-top"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Leo messi</h5>
+              <p className="card-text">
+              A new FIFA 23 leak points towards star footballer Lionel Messi getting a Flashback SBC card in Ultimate Team. 
+              Now, it seems like to might get the chance to get a special Lionel Messi Flashback card, which will likely be part of a Squad Building Challenge (SBC). 
+              </p>
+              <a href="" className="btn btn-outline-success btn-sm">
+                Read More
+              </a>
+              <a href="" className="btn btn-outline-danger btn-sm">
+                <i className="far fa-heart" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4 mb-4">
+          <div className="card">
+            <img
+              src="https://s3-eu-central-1.amazonaws.com/www-staging.esports.com/wp-media-folder-esports-com/app/uploads/2022/01/Varane-Flashback-FIFA-22.jpg"
+            
+              alt=""
+              className="card-img-top"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Varane</h5>
+              <p className="card-text">
+              The first TOTY releases are already out and about in FIFA 22′ Ultimate Team. As a bonus EA Sports has now brought back Flashback cards of former TOTY winners like Harry Kane (FIFA 18). Now leaks have surfaced about a defenisve powerhouse – Raphael Varane.
+
+OFFICIAL Stats for FLASHBACK VARANE
+Twitter user FUTSheriff has leaked the stats for the upcoming Flashback Varane. Since he had been spot on for the Team of the Year, the chances of him being accurate are high.
+
+
+              </p>
+              <a href="" className="btn btn-outline-success btn-sm">
+                Read More
+              </a>
+              <a href="" className="btn btn-outline-danger btn-sm">
+                <i className="far fa-heart" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4 mb-4">
+          <div className="card">
+            <img
+              src="https://s3-eu-central-1.amazonaws.com/www-staging.esports.com/wp-media-folder-esports-com/app/uploads/2022/02/fifa-22-neymar-flashback-sbc.jpg"
+              alt=""
+              className="card-img-top"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Neymar Jr</h5>
+              <p className="card-text">
+              Flashback SBC of Neymar Jr.
+Neymar Jr. has become one of the modern superstars in the football world. Ever since he transferred to FC Barcelona from FC Santos in 2013 after a stunning four years with 70 goals in Brazil’s Série A. A feat now honored by EA through this Flashback card.
+
+Although the card has four points less in total, it seems to fit the current FIFA 22 META better.
+              </p>
+              <a href="" className="btn btn-outline-success btn-sm">
+                Read More
+              </a>
+              <a href="" className="btn btn-outline-danger btn-sm">
+                <i className="far fa-heart" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  </div>
+</>
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
